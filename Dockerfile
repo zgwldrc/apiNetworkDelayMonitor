ea982a1770025901514e374455f9f5e478126a55
zgwldrc <zgwldrc@163.com>
2020-08-28 10:38:52 +0800

    m

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM golang
-WORKDIR /src
+WORKDIR /etc/apiNetworkDelayMonitor/
 ADD . .
 RUN go mod tidy && \
-    go build -o dist/app
+    go install
+ENTRYPOINT ["apiNetworkDelayMonitor"]
 
-FROM alpine
-WORKDIR /app
-COPY --from=0 /src/config.yaml /app/
-COPY --from=0 /src/dist/app    /app/
-EXPOSE 8080
-ENTRYPOINT ["/app/app"]
+
+#FROM alpine
+#WORKDIR /app
+#COPY --from=0 /src/config.yaml /app/
+#COPY --from=0 /src/dist/app    /app/
+#EXPOSE 8080
+#ENTRYPOINT ["/app/app"]
--- main.go
+++ main.go
@@ -6,25 +6,28 @@ import (
 	"github.com/spf13/viper"
 	"io"
 	"io/ioutil"
-	"log"
+
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"time"
 )
 
 type Route struct {
-	Path string
+	Path    string
 	Handler http.HandlerFunc
 }
 type Routes []Route
+
 var MyRoutes Routes
-func init () {
-	viper.SetConfigName("config") // name of config file (without extension)
-	viper.SetConfigType("yaml") // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("/etc/apiNetworkDelayMonitor/")   // path to look for the config file in
-	viper.AddConfigPath("$HOME/.apiNetworkDelayMonitor")  // call multiple times to add many search paths
-	viper.AddConfigPath(".")          // optionally look for config in the working directory
-	err := viper.ReadInConfig() // Find and read the config file
-	if err != nil { // Handle errors reading the config file
+
+func init() {
+	viper.SetConfigName("config")                        // name of config file (without extension)
+	viper.SetConfigType("yaml")                          // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("/etc/apiNetworkDelayMonitor/")  // path to look for the config file in
+	viper.AddConfigPath("$HOME/.apiNetworkDelayMonitor") // call multiple times to add many search paths
+	viper.AddConfigPath(".")                             // optionally look for config in the working directory
+	err := viper.ReadInConfig()                          // Find and read the config file
+	if err != nil {                                      // Handle errors reading the config file
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			// Config file not found; ignore error if desired
 			panic(fmt.Errorf("Fatal error config file: %s \n", err))
@@ -60,7 +63,7 @@ func main() {
 }
 
 func Frame(url string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request){
+	return func(w http.ResponseWriter, r *http.Request) {
 		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
 			log.Fatal(err)
@@ -70,7 +73,16 @@ func Frame(url string) http.HandlerFunc {
 		start := time.Now()
 		res, err := client.Do(req)
 		if err != nil {
-			log.Fatal(err)
+			statsFailedResponse := StatsFailedResponse{
+				Target: url,
+				ErrMsg: err.Error(),
+			}
+			respBody, _ := json.MarshalIndent(statsFailedResponse, "", "  ")
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(respBody)
+			log.Error(err)
+			return
 		}
 		end := time.Now()
 		duration := end.Sub(start)
@@ -79,17 +91,22 @@ func Frame(url string) http.HandlerFunc {
 		}
 		res.Body.Close()
 		statsResponse := StatsResponse{
-			Target: url,
+			Target:       url,
 			Milliseconds: duration.Milliseconds(),
 		}
 
 		respBody, _ := json.MarshalIndent(statsResponse, "", "  ")
+		w.Header().Set("Content-Type", "application/json")
 		w.Write(respBody)
 	}
 
 }
 
 type StatsResponse struct {
-	Target           string `json:"Target"`
-	Milliseconds 	 int64 `json:"Milliseconds"`
+	Target       string `json:"Target"`
+	Milliseconds int64  `json:"Milliseconds"`
+}
+type StatsFailedResponse struct {
+	Target string `json:"Target"`
+	ErrMsg string `json:"ErrMsg"`
 }
